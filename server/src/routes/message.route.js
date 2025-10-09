25798e579a17f813b1f1ab28c4c0c1ead91b31d5
Suyash Tripathi <suyatripathi03@gmail.com>
2025-10-09 10:03:10 +0530

    message-route added

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,9 +1,22 @@
 import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const messageRoute = express.Router();
 
-messageRoute.get("/send", (req, res) => {
-  res.send("send message route");
-});
+// the middle ware executed in sequence - so requests get rate limited first, then get authenticated.
+// this is actually more efficient as it saves unnecessary authentication checks for requests that are going to be blocked by rate limiting anyway.
+messageRoute.use(arcjetProtection, protectRoute);
+
+messageRoute.get("/contacts", getAllContacts);
+messageRoute.get("/chats", getChatPartners);
+messageRoute.get("/:id", getMessagesByUserId);
+messageRoute.post("/send/:id", sendMessage);
 
 export default messageRoute;
