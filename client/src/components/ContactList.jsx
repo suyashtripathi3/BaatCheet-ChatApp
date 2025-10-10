054978387289a673180f7aa5866cfc6a953da578
Suyash Tripathi <suyatripathi03@gmail.com>
2025-10-10 11:06:50 +0530

    sidebar implemented

--- client/src/components/ContactList.jsx
+++ client/src/components/ContactList.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore.js";
+import { useAuthStore } from "../store/useAuthStore.js";
+import UsersLoadingSkeleton from "./UsersLoadingSkeleton.jsx";
 
 function ContactList() {
+  const { getAllContacts, allContacts, setSelectedUser, isUsersLoading } =
+    useChatStore();
+  const { onlineUsers } = useAuthStore();
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+  if (isUsersLoading) return <UsersLoadingSkeleton />;
   return (
-    <div>
-      ContactList
-    </div>
-  )
+    <>
+      {allContacts.map((contact) => (
+        <div
+          key={contact._id}
+          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+          onClick={() => setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            {/* TODO: MAKE IT WORK WITH SOCKET */}
+            <div className={`avatar avatar-online`}>
+              <div className="size-12 rounded-full">
+                <img
+                  src={contact.profilePic || "/avatar.png"}
+                  alt={contact.fullName}
+                />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">
+              {contact.fullName}
+            </h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default ContactList
+export default ContactList;
