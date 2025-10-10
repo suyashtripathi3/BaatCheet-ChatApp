7f67b08c16b1ad9cc545eb2bcd84797c31d67f52
Suyash Tripathi <suyatripathi03@gmail.com>
2025-10-10 17:31:50 +0530

    chat container setup done

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore.js";
+import { useAuthStore } from "../store/useAuthStore.js";
+import ChatHeader from "./ChatHeader.jsx";
+import NoChatHistoryPlaceholder from "./NoChatHistoryPlaceholder .jsx";
+import MessageInput from "./MessageInput.jsx";
+import MessagesLoadingSkeleton from "./MessagesLoadingSkeleton.jsx";
 
 function ChatContainer() {
+  const { selectedUser, getMessagesByUserId, messages, isMessagesLoading } =
+    useChatStore();
+  const { authUser } = useAuthStore();
+
+  useEffect(() => {
+    getMessagesByUserId(selectedUser._id);
+  }, [selectedUser, getMessagesByUserId]);
+
   return (
-    <div>
-      ChatContainer
-    </div>
-  )
+    <>
+      <ChatHeader />
+      <div className="flex-1 px-6 overflow-y-auto py-8 ">
+        {messages.length > 0 && !isMessagesLoading ? (
+          <div className="max-w-3xl mx-auto space-y-6">
+            {messages.map((msg) => (
+              <div
+                key={msg._id}
+                className={`chat ${
+                  msg.senderId === authUser._id ? "chat-end" : "chat-start"
+                }`}
+              >
+                <div
+                  className={`chat-bubble relative ${
+                    msg.senderId === authUser._id
+                      ? "bg-cyan-600 text-white"
+                      : "bg-slate-800 text-slate-200"
+                  }`}
+                >
+                  {msg.image && (
+                    <img
+                      src={msg.image}
+                      alt="Shared"
+                      className="rounded-lg h-48 object-cover"
+                    />
+                  )}
+                  {msg.text && <p className="mt-2">{msg.text}</p>}
+                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
+                    {new Date(msg.createdAt).toISOString().slice(11, 16)}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : isMessagesLoading ? (
+          <MessagesLoadingSkeleton />
+        ) : (
+          <NoChatHistoryPlaceholder name={selectedUser.fullName} />
+        )}
+      </div>
+      <MessageInput />
+    </>
+  );
 }
 
-export default ChatContainer
+export default ChatContainer;
